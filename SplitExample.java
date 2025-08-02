cbfe2d5d5ab96ccb0e7750bd14c49e0454fb00ea
divyasreenidamanuri <nidamanuridivyasree@gmail.com>
2025-08-02 11:16:50 +0530

    Add files via upload

---
+++ SplitExample.java
@@ -0,0 +1,12 @@
+package oopsskill;
+
+public class SplitExample {
+	public static void main(String[] args) {
+        String s = "apple,banana,grape,orange";
+        String[] fruits = s.split(",");
+        for(String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+
+}
---
+++ SubstringExample12.java
@@ -0,0 +1,10 @@
+package oopsskill;
+
+public class SubstringExample12 {
+	public static void main(String[] args) {
+        String s1 = "JavaProgramming";
+        String sub = s1.substring(4, 11); // "Program"
+        System.out.println("Substring: " + sub);
+    }
+
+}
