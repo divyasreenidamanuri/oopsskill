10235be08571607d75180871dddb7afd22fda362
divyasreenidamanuri <nidamanuridivyasree@gmail.com>
2025-08-02 11:08:50 +0530

    Add files via upload

---
+++ TrimWhitespace.java
@@ -0,0 +1,9 @@
+package oopsskill;
+
+public class TrimWhitespace {
+	public static void main(String[] args) {
+        String s = "  Hello  ";
+        System.out.println("[" + s.trim() + "]");     
+    }
+
+}
